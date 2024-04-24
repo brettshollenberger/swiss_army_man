1531aaac1b88547e176c832a6477e63a84b448be
Brett Shollenberger <brettshollenberger@Bretts-MacBook-Pro-2.local>
2024-04-24 15:29:17 -0400

    0.1.31

--- swiss_army_man/ml/zipf_generator.py
+++ swiss_army_man/ml/zipf_generator.py
@@ -7,9 +7,14 @@ from scipy.stats import zipf
 # The Zipf generator takes a list and allows you to sample from it following the zipf distribution, returning
 # a random choice based on this law.
 class ZipfGenerator():
-    def __init__(self, items, s=1.5):
+    # After previously generating and saving a Zipf distribution as popularity ranks,
+    # you can pass this in in the future in order to generate new data
+    def __init__(self, items, popularity_ranks=None, s=1.5):
         self.items = items
-        self.popularity_ranks = zipf.rvs(s, size=len(items))
+        if popularity_ranks is not None:
+            self.popularity_ranks = popularity_ranks
+        else:
+            self.popularity_ranks = zipf.rvs(s, size=len(items))
 
     def random(self):
         return np.random.choice(self.items, p=self.popularity_ranks/self.popularity_ranks.sum())
