658373edd8d946f69d670aa30d3ff81b0df4f766
Brett Shollenberger <brettshollenberger@Bretts-MacBook-Pro-2.local>
2024-04-29 17:58:48 -0400

    0.1.52

--- swiss_army_man/ml/__init__.py
+++ swiss_army_man/ml/__init__.py
@@ -2,3 +2,5 @@ from .asset_preprocessor import AssetPreprocessor
 from .analyze import *
 from .date_splitter import *
 from .zipf_generator import ZipfGenerator
+from .normalize import *
+from .guassian_generator import GuassianGenerator
